50434bb8847a4f4fa30cad2f7141a5fc1e70915d
mehekj <mehek.jethani@gmail.com>
2023-06-15 23:15:04 -0700

    url fix

--- src/spot_api.py
+++ src/spot_api.py
@@ -8,6 +8,6 @@ def create_spotify_oauth():
     return SpotifyOAuth(
         client_id=os.environ.get("CLIENT_ID"),
         client_secret=os.environ.get("CLIENT_SECRET"),
-        redirect_uri=url_for("/spotify_utils/redirect_page", _external=True),
+        redirect_uri=url_for("redirect_page", _external=True),
         scope="user-library-read user-library-modify playlist-modify-public"
     )
