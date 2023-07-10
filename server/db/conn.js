9e51c45de67c4e8102c53f517baca55f3449106e
mehekj <mehek.jethani@gmail.com>
2023-07-09 23:27:04 -0700

    init

---
+++ server/db/conn.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.MONGO_URI || "";
+
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+  conn = await client.connect();
+} catch (err) {
+  console.error(err);
+}
+
+let db = conn.db("spotutils");
+
+export default db;
---
+++ server/routes/record.js
@@ -0,0 +1,11 @@
+import express from "express";
+import db from "../db/conn.js";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.send("test").status(200);
+});
+
+export default router;
