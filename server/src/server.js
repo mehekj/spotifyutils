608c3033bb95055208c9a6b6f76c5a47c82f7fb4
mehekj <mehek.jethani@gmail.com>
2023-07-10 22:06:54 -0700

    refactor

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.js";
-import records from "./routes/record.js";
+import records from "./routes.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
