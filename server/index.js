8bc656dfa2f26f14a2303f9778683bfd0e472f70
mehekj <mehek.jethani@gmail.com>
2023-07-13 23:04:07 -0700

    route update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
 import "./loadEnvironment.js";
-import records from "./routes.js";
+import router from "./routes.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", records);
+app.use("/record", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
