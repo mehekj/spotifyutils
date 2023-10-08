1127a20ea640fc548fc235f03c548f99057e816c
mehekj <mehek.jethani@gmail.com>
2023-10-08 00:52:45 -0400

    server isn't updating??

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import cors from "cors";
 import express from "express";
 import "./loadEnvironment.js";
 import router from "./routes.js";
-import { config } from "./constants.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
