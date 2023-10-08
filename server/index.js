e7ac832d656bb9e75fa07eadbdcd6549276524c1
mehekj <mehek.jethani@gmail.com>
2023-10-08 00:31:39 -0400

    cors let me in challenge

--- server/index.js
+++ server/index.js
@@ -6,7 +6,18 @@ import router from "./routes.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: [
+			"http://localhost:3000",
+			"https://localhost:3000",
+			"http://spotifyutils.mehekjethani.com",
+			"https://spotifyutils.mehekjethani.com",
+			"http://www.spotifyutils.mehekjethani.com",
+			"https://www.spotifyutils.mehekjethani.com",
+		],
+	})
+);
 app.use(express.json());
 
 app.use("/", router);
