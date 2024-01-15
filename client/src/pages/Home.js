5de7c84f66ebf9006ee9047d6d370716e08b3353
mehekj <mehek.jethani@gmail.com>
2024-01-15 01:02:54 -0500

    fix for new users

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,15 @@
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import { Heading, Link, Text, VStack } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../App";
-import { getTop20 } from "../spotify";
 import JSONTable from "../components/JSONTable";
+import { getTop20 } from "../spotify";
 
 export default function Home() {
 	const { user } = useContext(UserContext);
 	const [top20, setTop20] = useState([]);
 
 	useEffect(() => {
-		if (user.id) {
+		if (user.id && user.lastUpload) {
 			const fetchTop20 = async () => {
 				getTop20(user.id).then((res) => setTop20(res));
 			};
@@ -22,10 +22,17 @@ export default function Home() {
 			<Heading size={"2xl"}>
 				Welcome{user.display_name ? " " + user.display_name : ""}!
 			</Heading>
-			<Text>
-				Your last data upload:{" "}
-				{user.lastUpload && new Date(user.lastUpload).toLocaleString()}
-			</Text>
+			{user.lastUpload ? (
+				<Text>
+					Your last data upload:{" "}
+					{user.lastUpload && new Date(user.lastUpload).toLocaleString()}
+				</Text>
+			) : (
+				<Text>
+					Looks like you haven't uploaded any data yet. Add your files{" "}
+					<Link href="/upload">here</Link>.
+				</Text>
+			)}
 			{top20.length > 0 && (
 				<JSONTable data={top20} keys={["track", "artist", "liked"]} />
 			)}
