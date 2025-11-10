5f01ccb984bb1cbf097a36f76d406710fa9f5c8f
mehekj <mehek.jethani@gmail.com>
2025-11-09 23:11:28 -0800

    remove unused

--- client/src/api_utils.js
+++ client/src/api_utils.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
-
 export const api = axios.create({
 	withCredentials: true,
 });
@@ -22,67 +20,3 @@ api.interceptors.response.use(
 		return Promise.reject(error);
 	}
 );
-
-export const useApi = (requestFunc) => {
-	const [data, setData] = useState(null);
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		let isMounted = true;
-
-		const fetchData = async () => {
-			setLoading(true);
-			setError(null);
-
-			try {
-				const res = await requestFunc(api);
-				if (isMounted) {
-					setData(res.data);
-				}
-			} catch (err) {
-				if (!err._handled && isMounted) {
-					setError(err);
-				}
-			} finally {
-				if (isMounted) {
-					setLoading(false);
-				}
-			}
-		};
-
-		fetchData();
-
-		return () => {
-			isMounted = false;
-		};
-	}, [requestFunc]);
-
-	return { data, error, loading };
-};
-
-export const useApiMutation = () => {
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(false);
-
-	const mutate = useCallback(async (requestFunc) => {
-		setLoading(true);
-		setError(null);
-
-		try {
-			const res = await requestFunc(api);
-			return res.data;
-		} catch (err) {
-			if (!err._handled) {
-				setError(err);
-			}
-			throw err;
-		} finally {
-			setLoading(false);
-		}
-	}, []);
-
-	return { mutate, error, loading };
-};
-
-// todo add refresh token handling
